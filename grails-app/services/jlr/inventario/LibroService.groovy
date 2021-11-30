9bf37ba6ee457007af6d8ada426331943c581479
Juan López <juanlopez633@gmail.com>
2021-11-30 08:48:58 +0100

    servicio para la clase de dominio Libro

---
+++ grails-app/services/jlr/inventario/LibroService.groovy
@@ -0,0 +1,29 @@
+package jlr.inventario
+
+class LibroService {
+        
+    Libro get(Serializable id) {
+        Libro.get(id)
+    }
+    
+    List<Libro> list(Map args) {
+        Libro.list(args)
+    }
+    
+    Long count() {
+        Libro.count()
+    }
+
+    void delete(Serializable id) {
+
+    }
+
+    Libro save(Libro libro) {
+        libro.save(flush: true)        
+    }
+    
+    Libro findByTituloLike(String titulo) {
+        Libro.findByTituloLike("%${titulo}%")
+    }
+
+}
