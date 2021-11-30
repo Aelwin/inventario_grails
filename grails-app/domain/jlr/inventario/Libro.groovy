94489bbfaa81a997a424a2a4abd1617827840ab4
Juan López <juanlopez633@gmail.com>
2021-11-30 08:44:55 +0100

    clase de dominio para los libros

--- grails-app/domain/jlr/inventario/Libro.groovy
+++ grails-app/domain/jlr/inventario/Libro.groovy
@@ -2,6 +2,51 @@ package jlr.inventario
 
 class Libro {
 
+    String titulo
+	String propietario	
+	Date fechaInicioLectura
+	Date fechaFinLectura
+	String isbn
+	String sinopsis
+	int valoracion
+	String imagen
+	String editorial
+	Date fechaCompra
+	String observaciones
+	String categoria
+	Formato formato
+	Idioma idioma
+	Prestamo prestamo
+
+	static belongsTo = Autor
+	static hasMany = [autores: Autor]
+
+	static transients = ['leido']	
+
     static constraints = {
+    	fechaInicioLectura nullable: true
+    	fechaFinLectura nullable: true
+    	isbn nullable: true
+    	sinopsis nullable: true
+    	valoracion nullable: true, range: 1..10
+    	imagen nullable: true
+    	editorial nullable: true
+    	fechaCompra nullable: true
+    	observaciones nullable: true
+    	categoria nullable: true
+    	prestamo nullable: true
     }
+
+    static mapping = {		
+		version false
+	}
+
+	SiNo getLeido() {
+		fechaFinLectura ? SiNo.SI : SiNo.NO
+	}
+
+	@Override
+	String toString() {
+		"${titulo}"
+	}
 }
