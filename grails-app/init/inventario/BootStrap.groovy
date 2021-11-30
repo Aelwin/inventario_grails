f425c9ae8b166503b26e83564a392dcdee08f5ff
Juan López <juanlopez633@gmail.com>
2021-11-30 08:49:24 +0100

    cargamos algunos datos iniciales en la BBDD

--- grails-app/init/inventario/BootStrap.groovy
+++ grails-app/init/inventario/BootStrap.groovy
@@ -1,8 +1,39 @@
 package inventario
 
+import jlr.inventario.AutorService
+import jlr.inventario.Autor
+import jlr.inventario.Libro
+import jlr.inventario.enumerado.Formato
+import jlr.inventario.enumerado.Idioma
+
 class BootStrap {
 
+	AutorService autorService
+
     def init = { servletContext ->
+    	Autor tolkien = new Autor(nombre: "J.R.R. Tolkien")
+    	Autor sanderson = new Autor(nombre: "Brandon Sanderson")
+    	Autor asimov = new Autor(nombre: "Isaac Asimov")
+    	Autor preston = new Autor(nombre: "Douglas Preston")
+    	Autor child = new Autor(nombre: "Lincoln Child")
+    	tolkien.addToLibros(new Libro(titulo: "El señor de los anillos", propietario: "Izaskun", valoracion: 9,
+    		formato: Formato.FISICO, idioma: Idioma.CASTELLANO))
+    	sanderson.addToLibros(new Libro(titulo: "El camino de los reyes", propietario: "Juan", valoracion: 10,
+    		formato: Formato.DIGITAL, idioma: Idioma.CASTELLANO))
+    	sanderson.addToLibros(new Libro(titulo: "Palabras radiantes", propietario: "Juan", valoracion: 10,
+    		formato: Formato.DIGITAL, idioma: Idioma.CASTELLANO))
+    	asimov.addToLibros(new Libro(titulo: "Fundación", propietario: "Juan", valoracion: 9,
+    		formato: Formato.FISICO, idioma: Idioma.CASTELLANO))
+    	Libro elRelicario = new Libro(titulo: "El relicario", propietario: "Izaskun", valoracion: 7,
+    		formato: Formato.FISICO, idioma: Idioma.CASTELLANO)
+    	preston.addToLibros(elRelicario)
+    	child.addToLibros(elRelicario)
+    	
+    	autorService.save(tolkien)
+    	autorService.save(sanderson)
+    	autorService.save(asimov)
+    	autorService.save(preston)
+    	autorService.save(child)
     }
     def destroy = {
     }
