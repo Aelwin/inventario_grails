7d59f47d9332298b85f69c276cff1043c80b8273
Juan López <juanlopez633@gmail.com>
2021-12-23 13:22:24 +0100

    nuevo metodo para filtrar libros

--- grails-app/services/jlr/inventario/LibroService.groovy
+++ grails-app/services/jlr/inventario/LibroService.groovy
@@ -1,10 +1,13 @@
 package jlr.inventario
 
 import grails.gorm.transactions.Transactional
+import grails.gorm.transactions.NotTransactional
+import jlr.inventario.enumerado.Formato
+import jlr.inventario.enumerado.Idioma
 
 @Transactional
 class LibroService {
-        
+    
     Libro get(Serializable id) {
         Libro.get(id)
     }
@@ -24,9 +27,58 @@ class LibroService {
     Libro save(Libro libro, validar = true) {
         libro.save(flush: true, validate: validar)
     }
-    
+
+    @NotTransactional
     Libro findByTituloLike(String titulo) {
         Libro.findByTituloLike("%${titulo}%")
+    }
+
+    @NotTransactional
+    def filtrarLibros(FiltroCommand filtroBusqueda) {
+        println "A:${filtroBusqueda.autores*.id}"
+        def criteria = Libro.createCriteria()        
+        def resultado = criteria.list(max: filtroBusqueda.max, offset: filtroBusqueda.offset) {            
+            and {
+                if (filtroBusqueda.titulo) {
+                    log.debug "Filtramos por el titulo ${filtroBusqueda.titulo}"
+                    ilike('titulo', "%${filtroBusqueda.titulo}%")
+                }
+                if (filtroBusqueda.autores) {
+                    log.debug "Filtramos por los autores ${filtroBusqueda.autores}"
+                    autores {
+                        'in'('id', filtroBusqueda.autores*.id)
+                    }                    
+                }
+                if (filtroBusqueda.propietario) {
+                    log.debug "Filtramos por el propietario ${filtroBusqueda.propietario}"
+                    ilike('propietario', "%${filtroBusqueda.propietario}%")
+                }
+                if (filtroBusqueda.fechaInicioLectura || filtroBusqueda.fechaFinLectura) {
+                    Date fechaDesdeSinHora = filtroBusqueda.fechaInicioLectura?.clearTime()
+                    filtroBusqueda.fechaFinLectura = filtroBusqueda.fechaFinLectura?: new Date()
+                    log.debug "Filtramos entre las fechas de lectura ${filtroBusqueda.fechaInicioLectura}-${filtroBusqueda.fechaFinLectura}"
+                    Date fechaHastaSinHora = filtroBusqueda.fechaFinLectura.clearTime()
+                    if (fechaDesdeSinHora)
+                        ge('fechaInicioLectura', fechaDesdeSinHora)
+                    lt('fechaInicioLectura', fechaHastaSinHora + 1)
+                }
+                if (filtroBusqueda.categoria) {
+                    log.debug "Filtramos por la categoria ${filtroBusqueda.categoria}"
+                    ilike('propietario', "%${filtroBusqueda.categoria}%")
+                }
+                if (filtroBusqueda.formato) {
+                    log.debug "Filtramos por el formato ${filtroBusqueda.formato}"
+                    eq('formato', Formato.values().find{ it == filtroBusqueda.formato })
+                }
+                if (filtroBusqueda.idioma) {
+                    log.debug "Filtramos por el idioma ${filtroBusqueda.idioma}"
+                    eq('idioma', Idioma.values().find{ it == filtroBusqueda.idioma })
+                }
+                
+            }
+            order("titulo", "asc")
+        }
+        resultado
     }
 
 }
