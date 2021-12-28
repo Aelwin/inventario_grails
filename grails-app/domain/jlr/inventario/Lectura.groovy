b99a057d6ce2a6d189abb0d21c34a1a1426cc616
Juan López <juanlopez633@gmail.com>
2021-12-28 13:45:03 +0100

    nueva clase de dominio Lectura

---
+++ grails-app/domain/jlr/inventario/Lectura.groovy
@@ -0,0 +1,42 @@
+package jlr.inventario
+
+class Lectura {
+
+	String lector
+	Date fechaInicio
+	Date fechaFin
+	Integer valoracion
+
+	static belongsTo = [libro: Libro]
+
+    static constraints = {
+    	fechaFin nullable: true, validator: { Date val, Lectura obj -> obj.validarFechaFinMayorIgualFechaInicio(val, obj) }
+    	valoracion nullable: true, range: 1..5, validator: { Integer val, Lectura obj -> obj.validarHaSidoLeido(val, obj) }
+    }
+
+    static mapping = {		
+		version false
+		id generator: 'increment'
+	}
+
+	def beforeValidate() {
+		//Si no se asigna un lector asumimos que es el propietario del libro
+		if (!lector) {
+			lector = libro.propietario
+		}
+		valoracion = valoracion == 0 ? null : valoracion
+	}
+
+	def validarFechaFinMayorIgualFechaInicio(valorFechaFin, instancia) {
+		if (valorFechaFin && valorFechaFin < instancia.fechaInicio) {
+			'menorFechaInicio'
+		}
+	}
+
+	def validarHaSidoLeido(valorValoracion, instancia) {
+		if (valorValoracion && !instancia.fechaFin) {
+			'libroNoLeido'
+		}
+	}
+
+}
